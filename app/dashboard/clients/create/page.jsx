3aa240027fb730aaa5942983ec0613ce605703b1
majzoub.malek <majzoub.malek@gmail.com>
2023-05-11 01:28:32 +0300

    edit readme

---
+++ app/dashboard/clients/create/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./page.module.css";
+
+const CreateClient = () => {
+    return (
+        <div>
+            <div>Create New Client</div>
+        </div>
+    );
+};
+
+export default CreateClient;
