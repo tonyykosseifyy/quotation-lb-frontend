0654976bcf31f31ae89c83e90f20f6d79bacaa1b
Abed Nahouli <abednahouli@gmail.com>
2023-05-27 01:27:44 +0300

    favicon and preview

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,35 @@
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { Inter } from "next/font/google";
-
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
     title: "Rooster",
     description: "Your business is ready to crow",
+    icons: {
+        icon: "/favicon.ico",
+    },
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <meta
+                    property="og:image"
+                    content="/favicon.ico"
+                />
+                <meta
+                    property="og:image:secure_url"
+                    content="/favicon.ico"
+                />
+            </Head>
             <body>{children}</body>
         </html>
     );
