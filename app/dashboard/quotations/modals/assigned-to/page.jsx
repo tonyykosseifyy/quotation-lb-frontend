26da98077f6141377f2543fbe3f7ec9c3b1b526a
Abed Nahouli <abednahouli@gmail.com>
2023-06-15 03:58:34 +0300

    removed inline svgs and put them inside .svg files

--- app/dashboard/quotations/modals/assigned-to/page.jsx
+++ app/dashboard/quotations/modals/assigned-to/page.jsx
@@ -17,20 +17,23 @@ const options = [
 const assignedToModal = () => {
     const [showModal, setShowModal] = useState(true);
 
-    const modalStyle = { 
-        overlay: { 
-            backgroundColor: "var(--modal-overlay-background-clr)"
-        }, 
-         content: { 
-            top: '53%', left: '50%', right: 'auto', bottom: 'auto',
-            transform: 'translate(-50%, -50%)',
-            width: "88%", 
+    const modalStyle = {
+        overlay: {
+            backgroundColor: "var(--modal-overlay-background-clr)",
+        },
+        content: {
+            top: "53%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            transform: "translate(-50%, -50%)",
+            width: "88%",
             height: "auto",
             padding: "40px 44px 34px 45px",
             borderRadius: "8px",
             borderTop: "2px solid var(--primary-clr)",
-        } 
-    };      
+        },
+    };
 
     const {
         register,
@@ -71,109 +74,91 @@ const assignedToModal = () => {
 
     return (
         <div className={`container m-0`}>
-            <div
-                className={styles.title}
-                 onClick={() => setShowModal(true)}
-                >
-                Assigned To 
+            <div className={styles.title} onClick={() => setShowModal(true)}>
+                Assigned To
             </div>
             <ModalComponent
                 title="Create Assigned To"
-                titlePaddingBottom="40px" 
-                isOpen={showModal} 
-                onRequestClose={() => setShowModal(false)} 
+                titlePaddingBottom="40px"
+                isOpen={showModal}
+                onRequestClose={() => setShowModal(false)}
                 style={modalStyle}
             >
                 <form id="assignedTo" onSubmit={handleSubmit(onSubmit)}>
-                      <div className={`d-flex ${styles.circleAndInputsContainer}`}>
-                           <div className={styles.bigCircle}>
-                                <div className={styles.smallCircle}>
-                                   <svg
-                                      width="20"
-                                      height="20"
-                                      viewBox="0 0 20 20"
-                                      fill="none"
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      >
-                                      <path
-                                       d="M17.3938 11.2322H11.232V17.394C11.232 17.7209 11.1021 18.0343 10.871 18.2655C10.6399 18.4966 10.3264 18.6264 9.9996 18.6264C9.67276 18.6264 9.3593 18.4966 9.12819 18.2655C8.89708 18.0343 8.76724 17.7209 8.76724 17.394V11.2322H2.60541C2.27857 11.2322 1.96511 11.1024 1.734 10.8713C1.50289 10.6401 1.37305 10.3267 1.37305 9.99985C1.37305 9.673 1.50289 9.35955 1.734 9.12843C1.96511 8.89732 2.27857 8.76748 2.60541 8.76748H8.76724V2.60566C8.76724 2.27881 8.89708 1.96536 9.12819 1.73424C9.3593 1.50313 9.67276 1.37329 9.9996 1.37329C10.3264 1.37329 10.6399 1.50313 10.871 1.73424C11.1021 1.96536 11.232 2.27881 11.232 2.60566V8.76748H17.3938C17.7206 8.76748 18.0341 8.89732 18.2652 9.12843C18.4963 9.35955 18.6262 9.673 18.6262 9.99985C18.6262 10.3267 18.4963 10.6401 18.2652 10.8713C18.0341 11.1024 17.7206 11.2322 17.3938 11.2322Z"
-                                       fill="var(--primary-clr-light)"
-                                       stroke="var(--primary-clr-light)"
-                                       strokeWidth="1.23237"
-                                       />
-                                 </svg>
-                             </div> 
-                         </div>
-                         <div className={`${styles.inputsContainer}`}>
-                               <div className={`${styles.inputRow2}`}>
-                                   <InputContainer
-                                       label="Name"
-                                       isRequired={true}
-                                       inputPlaceholder="Search..."
-                                       inputType="select"
-                                       inputName="name"
-                                       selectOptions={options}
-                                       register={register}
-                                       control={control}
-                                    />
-                                   <InputContainer
-                                       label="Email"
-                                       isRequired={true}
-                                       inputPlaceholder=""
-                                       inputType="text"
-                                       inputName="email"
-                                       register={register}
-                                       control={control}
-                                   />
-                                    <InputContainer
-                                       label="Phone"
-                                       isRequired={true}
-                                       inputPlaceholder=""
-                                       inputType="phone"
-                                       codeName="phoneCode"
-                                       changeCodeValue={changeCodeValue}
-                                       inputName="phone"
-                                       selectOptions={PhoneCodeSelect}
-                                       register={register}
-                                       control={control}
-                                    />
-                                    <InputContainer
-                                       label="Mobile"
-                                       isRequired={true}
-                                       inputPlaceholder=""
-                                       inputType="phone"
-                                       codeName="mobileCode"
-                                       changeCodeValue={changeCodeValue}
-                                       inputName="mobile"
-                                       selectOptions={options}
-                                       register={register}
-                                       control={control}
-                                    />
-                             </div>
-                         </div>
-                      </div>
-                     <div className={`${styles.actionButtons2}`}>
-                            <div
-                                className={styles.discard}
-                                onClick={() => reset()}
-                                >
-                                Discard
+                    <div
+                        className={`d-flex ${styles.circleAndInputsContainer}`}
+                    >
+                        <div className={styles.bigCircle}>
+                            <div className={styles.smallCircle}>
+                                <img src="/assets/svg/plus-blue.svg" alt="" />
                             </div>
-                            <Button
-                                title="Save"
-                                rounded={false}
-                                fillBackground={true}
-                                paddingTop={10}
-                                paddingBottom={10}
-                                paddingRight={65}
-                                paddingLeft={65}
-                            />
-                     </div>
-             </form>
-         </ModalComponent>
-     </div>
+                        </div>
+                        <div className={`${styles.inputsContainer}`}>
+                            <div className={`${styles.inputRow2}`}>
+                                <InputContainer
+                                    label="Name"
+                                    isRequired={true}
+                                    inputPlaceholder="Search..."
+                                    inputType="select"
+                                    inputName="name"
+                                    selectOptions={options}
+                                    register={register}
+                                    control={control}
+                                />
+                                <InputContainer
+                                    label="Email"
+                                    isRequired={true}
+                                    inputPlaceholder=""
+                                    inputType="text"
+                                    inputName="email"
+                                    register={register}
+                                    control={control}
+                                />
+                                <InputContainer
+                                    label="Phone"
+                                    isRequired={true}
+                                    inputPlaceholder=""
+                                    inputType="phone"
+                                    codeName="phoneCode"
+                                    changeCodeValue={changeCodeValue}
+                                    inputName="phone"
+                                    selectOptions={PhoneCodeSelect}
+                                    register={register}
+                                    control={control}
+                                />
+                                <InputContainer
+                                    label="Mobile"
+                                    isRequired={true}
+                                    inputPlaceholder=""
+                                    inputType="phone"
+                                    codeName="mobileCode"
+                                    changeCodeValue={changeCodeValue}
+                                    inputName="mobile"
+                                    selectOptions={options}
+                                    register={register}
+                                    control={control}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`${styles.actionButtons2}`}>
+                        <div className={styles.discard} onClick={() => reset()}>
+                            Discard
+                        </div>
+                        <Button
+                            title="Save"
+                            rounded={false}
+                            fillBackground={true}
+                            paddingTop={10}
+                            paddingBottom={10}
+                            paddingRight={65}
+                            paddingLeft={65}
+                        />
+                    </div>
+                </form>
+            </ModalComponent>
+        </div>
     );
 };
 
 export default assignedToModal;
-  
--- components/Modal/Modal.jsx
+++ components/Modal/Modal.jsx
@@ -2,38 +2,32 @@
 
 import React from "react";
 import styles from "./Modal.module.css";
-import  Modal  from "react-modal";
+import Modal from "react-modal";
 
-const ModalComponent = ({ isOpen, onRequestClose, style, title, titlePaddingBottom, children }) => {
-  return (
-    <Modal 
-      isOpen={isOpen} 
-      onRequestClose={onRequestClose} 
-      style={style}
-    >
-      <div className={`${styles.xIcon}`}>
-         <svg 
-            width="22" 
-            height="21" 
-            viewBox="0 0 22 21" 
-            fill="none" 
-            xmlns="http://www.w3.org/2000/svg"
-            onClick={onRequestClose}
-          >
-            <path 
-                fill-rule="evenodd" 
-                clip-rule="evenodd" 
-                d="M21.6387 10.5C21.6387 16.2991 16.9378 21 11.1387 21C5.33952 21 0.638672 16.2991 0.638672 10.5C0.638672 4.70085 5.33952 0 11.1387 0C16.9378 0 21.6387 4.70085 21.6387 10.5ZM7.95717 7.3185C8.10483 7.17103 8.30498 7.08819 8.51367 7.08819C8.72236 7.08819 8.92251 7.17103 9.07017 7.3185L11.1387 9.387L13.2072 7.3185C13.3565 7.1794 13.5539 7.10367 13.7579 7.10727C13.9619 7.11087 14.1566 7.19351 14.3009 7.3378C14.4452 7.48208 14.5278 7.67674 14.5314 7.88075C14.535 8.08477 14.4593 8.28222 14.3202 8.4315L12.2517 10.5L14.3202 12.5685C14.4593 12.7178 14.535 12.9152 14.5314 13.1192C14.5278 13.3233 14.4452 13.5179 14.3009 13.6622C14.1566 13.8065 13.9619 13.8891 13.7579 13.8927C13.5539 13.8963 13.3565 13.8206 13.2072 13.6815L11.1387 11.613L9.07017 13.6815C8.92089 13.8206 8.72344 13.8963 8.51942 13.8927C8.31541 13.8891 8.12075 13.8065 7.97647 13.6622C7.83219 13.5179 7.74954 13.3233 7.74594 13.1192C7.74234 12.9152 7.81807 12.7178 7.95717 12.5685L10.0257 10.5L7.95717 8.4315C7.8097 8.28384 7.72686 8.08369 7.72686 7.875C7.72686 7.66631 7.8097 7.46616 7.95717 7.3185Z" 
-                fill="var(--primary-text-clr)"
-            />
-        </svg>
-      </div>
-      <div className={`${styles.modalTitle}`} style={{paddingBottom: titlePaddingBottom}}>
-          {title}
-      </div>  
-      {children}
-   </Modal>
-  )
-}
+const ModalComponent = ({
+    isOpen,
+    onRequestClose,
+    style,
+    title,
+    titlePaddingBottom,
+    children,
+}) => {
+    return (
+        <Modal isOpen={isOpen} onRequestClose={onRequestClose} style={style}>
+            <div className={`${styles.xIcon}`}>
+                <span onClick={onRequestClose}>
+                    <img src="/assets/svg/xIcon.svg" alt="close modal" />
+                </span>
+            </div>
+            <div
+                className={`${styles.modalTitle}`}
+                style={{ paddingBottom: titlePaddingBottom }}
+            >
+                {title}
+            </div>
+            {children}
+        </Modal>
+    );
+};
 
 export default ModalComponent;
