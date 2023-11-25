125bccd7229692966ce8e557f466777e5acee8d7
Malek Majzoub <majzoub.malek@gmail.com>
2023-11-25 16:26:30 +0200

    remove logs

--- api/axiosClient.js
+++ api/axiosClient.js
@@ -5,8 +5,6 @@ const axiosClient = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_SERVER_APP_BASE_URL}/api`,
 });
 
-console.log("base url", `${process.env.NEXT_PUBLIC_SERVER_APP_BASE_URL}/api`);
-
 axiosClient.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token;
   config.headers.Authorization = `Bearer ${token}`;
